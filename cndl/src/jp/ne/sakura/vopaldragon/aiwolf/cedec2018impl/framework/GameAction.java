b7352e4125bf90dd28978f1e789e07da440a3500
yoshinobc <pipbcmsi@gmail.com>
2019-03-05 21:21:45 +0900

    fix various

--- cndl/src/jp/ne/sakura/vopaldragon/aiwolf/cedec2018impl/framework/GameAction.java
+++
@@ -1,6 +0,0 @@
-package jp.ne.sakura.vopaldragon.aiwolf.cedec2018impl.framework;
-
-public enum GameAction {
-
-    DAYSTART, TALK, VOTE, REVOTE, WHISPER, ATTACKVOTE, ATTACKREVOTE, DIVINE, GUARD,;
-}
--- cndl/src/jp/ne/sakura/vopaldragon/aiwolf/cedec2018impl/framework/GameAfterActionListener.java
+++
@@ -1,8 +0,0 @@
-package jp.ne.sakura.vopaldragon.aiwolf.cedec2018impl.framework;
-
-import org.aiwolf.client.lib.Content;
-
-public interface GameAfterActionListener {
-
-    void handleAfterAction(Game g, GameAction action, Content talk, GameAgent target);
-}
